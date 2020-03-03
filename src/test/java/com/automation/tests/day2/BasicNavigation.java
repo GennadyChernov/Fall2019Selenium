b266ed9c657436a0a58c3e4e5b2728ac1982ab2d
GennadyChernov <58498342+GennadyChernov@users.noreply.github.com>
2020-03-03 16:27:12 -0500

    Basic Navigation

---
+++ src/test/java/com/automation/tests/day2/BasicNavigation.java
@@ -0,0 +1,23 @@
+package com.automation.tests.day2;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+public class BasicNavigation {
+    public static void main(String[] args) {
+        //to start selenium script we need
+        // set up webDriver (browser driver) and create webDriver object
+
+        WebDriverManager.chromedriver().setup();
+        //ChromeDriver driver = new ChromeDriver();
+        //in selenium, everything starts from Web driver Interface
+      //  RemoteWebDriver driver = new SafariDriver() ;
+        WebDriver driver = new ChromeDriver() ;
+        driver.get("https://www.google.com/"); // to open the website
+
+
+    }
+}
