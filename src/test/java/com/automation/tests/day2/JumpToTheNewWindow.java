d8a976b051c6ece50c90298621cd914d6227f4fb
GennadyChernov <58498342+GennadyChernov@users.noreply.github.com>
2020-03-16 16:47:13 -0400

    update

--- src/test/java/com/automation/tests/day2/JumpToTheNewWindow.java
+++ src/test/java/com/automation/tests/day2/JumpToTheNewWindow.java
@@ -8,7 +8,7 @@ import java.util.Set;
 
 public class JumpToTheNewWindow {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception{
         WebDriverManager.chromedriver().setup();
 
         WebDriver driver = new ChromeDriver();
@@ -22,7 +22,43 @@ public class JumpToTheNewWindow {
         String windowHandle = driver.getWindowHandle();
 
         System.out.println(windowHandle);
+        //getWindowHandles() - returns id's of all currently opened windows
+        //Set - doesn't allow duplicates
 
+        Set<String> windowHandles = driver.getWindowHandles();
 
+        System.out.println(windowHandles);
+        System.out.println("BEFORE SWITCH :"+driver.getCurrentUrl());
+        //since we have all windows
+        //and we know id of original window
+        //we can say switch to something that is not equals to old window id
+        for (String windowId: windowHandles){
+            //if it's not an old window, then switch
+            if(!windowId.equals(windowHandle)){
+                //to jump to the new window
+                driver.switchTo().window(windowId);
+            }
+        }
+        System.out.println("AFTER SWITCH :"+driver.getCurrentUrl());
+        // break until 5:02
+
+//        driver.close();
+        driver.quit();
+    }
+
+    /**
+     *
+     * This method helps to switch in between windows based on page title
+     * @param pageTitle
+     * @param driver
+     */
+    public static void switchToWindowBasedOnTitle(String pageTitle, WebDriver driver){
+        Set<String> windows = driver.getWindowHandles();
+        for (String window: windows){
+            driver.switchTo().window(window);
+            if (driver.getTitle().equals(pageTitle)){
+                break;
+            }
+        }
     }
 }
