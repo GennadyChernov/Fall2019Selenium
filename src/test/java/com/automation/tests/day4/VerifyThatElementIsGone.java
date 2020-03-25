43a9414dbf264e2d55d453a6f77e8a2a447c2547
GennadyChernov <58498342+GennadyChernov@users.noreply.github.com>
2020-03-25 18:22:26 -0400

    update

--- src/test/java/com/automation/tests/day4/VerifyThatElementIsGone.java
+++ src/test/java/com/automation/tests/day4/VerifyThatElementIsGone.java
@@ -1,5 +1,75 @@
 package com.automation.tests.day4;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
 public class VerifyThatElementIsGone {
 
+    /**
+     * Interview question:
+     *
+     * how to check if element doesn't exists any more in th DOM (Document Object Model (that HTML code))
+     *
+     */
+
+    public static void main(String[] args) throws Exception {
+
+        WebDriverManager.chromedriver().version("79").setup();
+        WebDriver driver = new ChromeDriver() ;
+        driver.get("http://practice.cybertekschool.com/multiple_buttons");
+        Thread.sleep(2000);
+
+        driver.findElement(By.id("disappearing_button")).click();
+        Thread.sleep(2000);
+
+        if (driver.findElements(By.id("disappearing_button")).size() == 0) {
+            System.out.println("Test Passed");
+        }else{
+            System.out.println("Test Failed");
+        }
+
+
+        List<WebElement> list = driver.findElements(By.id("disappearing_button"));
+
+        //if size is 0, that means no elements were found
+
+        if(list.size() == 0){
+            System.out.println("TEST PASSED");
+        }else {
+            System.out.println("TEST FAILED");
+        }
+
+        // another way is.Empty
+        if (list.isEmpty()){
+            System.out.println("yea");
+
+        }else {
+            System.out.println("no");
+        }
+
+
+        Thread.sleep(2000);
+        //to find all buttons
+        //make sure that you us findElements <--- ending is important
+        List<WebElement> buttons = driver.findElements(By.tagName("button"));
+
+        for(WebElement button: buttons){
+            //click on every button
+            button.click();
+            Thread.sleep(2000);
+        }
+
+
+
+
+driver.quit();
+
+
+
+    }
 }
